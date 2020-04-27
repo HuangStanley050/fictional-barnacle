cf4b6e201f6a4fced3e4e263262171a0cee4ad72
Stanley Huang <huang.stanley050@gmail.com>
2020-04-27 11:32:14 +1000

    using a quick setup

--- handler.js
+++ handler.js
@@ -1,14 +1,29 @@
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
-  };
-};
+import { GraphQLServerLambda } from "graphql-yoga";
 
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
+const typeDefs = `
+  type Query {
+    hello(name: String): String!
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: (_, { name }) => `Hello ${name || "World"}`,
+  },
+};
+const lambda = new GraphQLServerLambda({
+  typeDefs,
+  resolvers,
+  context: (req) => ({ ...req }),
+});
+
+exports.playground = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  //await start();
+  return lambda.playgroundHandler(event, context, callback);
+};
+exports.server = async (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  //await start();
+  return lambda.graphqlHandler(event, context, callback);
+};
