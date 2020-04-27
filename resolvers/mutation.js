7fee45d0c1bc959b4689cdce9ad471a022dc1c19
Stanley Huang <huang.stanley050@gmail.com>
2020-04-27 15:59:40 +1000

    sign up user

--- resolvers/mutation.js
+++ resolvers/mutation.js
@@ -1,16 +1,27 @@
 import { connectToDatabase } from "../db";
 import User from "../models/User";
+import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
+
 const mutation = {
   signup: async (parent, args, ctx, info) => {
-    await connectToDatabase();
-    return {
-      token: "stuff",
-      user: {
-        id: "1",
-        email: "test",
-        todos: [],
-      },
-    };
+    try {
+      await connectToDatabase();
+      let user = await User.findOne({ email: args.email }).lean();
+      if (user) {
+        throw new Error("Email is already taken");
+      }
+      const _password = await bcrypt.hash(args.password, 10);
+      user = await new User({
+        email: args.email,
+        password: _password,
+      }).save();
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET);
+      return { token, user };
+    } catch (err) {
+      console.log(err);
+      throw new Error("Something went wrong");
+    }
   },
 };
 
