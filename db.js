73d0856c27b987e401ec0cb97d4914f09122c634
Stanley Huang <huang.stanley050@gmail.com>
2020-04-27 15:37:35 +1000

    added database connect

---
+++ db.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+mongoose.Promise = global.Promise;
+
+let isConnected;
+const url = process.env.MONGO_URL;
+
+export const connectToDatabase = async () => {
+  if (isConnected) {
+    console.log("=> using existing database connection");
+    return;
+  }
+
+  console.log("=> using new database connection");
+  const db = await mongoose.connect(url);
+  isConnected = db.connections[0].readyState;
+  return;
+};
