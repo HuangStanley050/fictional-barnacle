c245d3208b2eed285db0c0f8232616e67fe0dda2
Stanley Huang <huang.stanley050@gmail.com>
2020-04-28 10:24:36 +1000

    add check token middleware

---
+++ checkAuth.js
@@ -0,0 +1,11 @@
+const authenticate = (context) => {
+  const Authorization = context.event.headers.Authorization;
+  if (Authorization) {
+    const token = Authorization.replace("Bearer ", "");
+    const { userId } = jwt.verify(token, process.env.JWT_SECRET);
+    return userId;
+  }
+  throw new Error("Not authorized");
+};
+
+export default authenticate;
