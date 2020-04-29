a7a8d13f6276a7c373460d001606cb63dcbb1989
Stanley Huang <huang.stanley050@gmail.com>
2020-04-29 12:10:15 +1000

    nested query done for todos

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,10 +1,22 @@
 import User from "../models/User";
 import Todo from "../models/Todo";
+import mongoose from "mongoose";
 
 const user = {
   todos: async (parent, args, ctx, info) => {
-    const todoIds = parent.todos;
-    console.log(todoIds);
+    let todoIds = parent.todos;
+    if (todoIds.length === 0) {
+      return null;
+    }
+    //console.log(todoIds);
+    todoIds = todoIds.map((id) => mongoose.Types.ObjectId(id));
+    let result = await Todo.find({
+      _id: {
+        $in: todoIds,
+      },
+    });
+    console.log(result);
+    return result;
   },
 };
 
