f8165f5ac667c874a6f639b8a710516794875773
Stanley Huang <huang.stanley050@gmail.com>
2020-04-29 11:50:45 +1000

    start on users query

--- resolvers/query.js
+++ resolvers/query.js
@@ -1,5 +1,13 @@
+import User from "../models/User";
+import { connectToDatabase } from "../db";
 const query = {
   hello: () => "hello",
+  users: async (parent, args, ctx, info) => {
+    await connectToDatabase();
+    let users = await User.find({});
+    console.log(users);
+    return users;
+  },
 };
 
 export default query;
--- resolvers/todo.js
+++ resolvers/todo.js
@@ -1,10 +1,12 @@
 import User from "../models/User";
 import Todo from "../models/Todo";
 
-const todo = {creator: async (parent,args,ctx,info)=>{
-  const creatorId = parent.creator;
-  let user = await User.findOne({_id:creatorId});
-  return user;                        
-}};
-                                            
-export default todo;                   
+const todo = {
+  creator: async (parent, args, ctx, info) => {
+    const creatorId = parent.creator;
+    let user = await User.findOne({ _id: creatorId });
+    return user;
+  },
+};
+
+export default todo;
