ed7fdfa3bf2e763cf058b60f8ba90ba37349ee1b
Stanley Huang <huang.stanley050@gmail.com>
2020-04-30 13:50:51 +1000

    trying data loader

--- resolvers/todo.js
+++ resolvers/todo.js
@@ -1,11 +1,19 @@
 import User from "../models/User";
 import Todo from "../models/Todo";
+import Dataloader from "dataloader";
+
+const batchGetUserById = async (ids) => {
+  const users = await User.find({});
+  return ids.map((id) => users.find((user) => user._id === id));
+};
+const userLoader = new DataLoader(batchGetUserById);
 
 const todo = {
   creator: async (parent, args, ctx, info) => {
-    const creatorId = parent.creator;
-    let user = await User.findOne({ _id: creatorId });
-    return user;
+    // const creatorId = parent.creator;
+    // let user = await User.findOne({ _id: creatorId });
+    // return user;
+    return userLoader.load(parent.creator);
   },
 };
 
